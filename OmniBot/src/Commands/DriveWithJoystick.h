6bc08c3be66e74fd7e69be8d789199acc16f488c
Michael Brock <dday8889@gmail.com>
2016-10-25 17:36:36 -0600

    Initial Commit for OmniBot

---
+++ OmniBot/src/Commands/DriveWithJoystick.h
@@ -0,0 +1,18 @@
+#ifndef DriveWithJoystick_H
+#define DriveWithJoystick_H
+
+#include "../CommandBase.h"
+#include "WPILib.h"
+
+class DriveWithJoystick: public CommandBase
+{
+public:
+	DriveWithJoystick();
+	void Initialize();
+	void Execute();
+	bool IsFinished();
+	void End();
+	void Interrupted();
+};
+
+#endif

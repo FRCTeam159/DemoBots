2e87966a328d9772a32034d2699a8df5dbb89102
JohnHarrell <40332@psdschools.org>
2016-11-01 17:23:12 -0600

    added shooter subsystem

---
+++ CommandDiscusBot/src/Subsystems/Shooter.cpp
@@ -0,0 +1,18 @@
+#include "Shooter.h"
+#include "../RobotMap.h"
+
+Shooter::Shooter() :
+		Subsystem("Shooter")
+{
+	flywheelMotor = new CANTalon(1);
+}
+
+void ExampleSubsystem::InitDefaultCommand()
+{
+	// Set the default command for a subsystem here.
+	//SetDefaultCommand(new MySpecialCommand());
+}
+
+// Put methods for controlling this subsystem
+// here. Call these from Commands.
+
